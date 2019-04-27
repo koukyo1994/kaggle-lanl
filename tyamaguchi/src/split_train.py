c997a6a9fea9efa848b404283317b0266d33b48a
山口大器 <sunflower6432779@gmail.com@>
2019-04-28 01:55:11 +0900

    bug fix

--- tyamaguchi/src/split_train.py
+++ tyamaguchi/src/split_train.py
@@ -17,20 +17,24 @@ def main():
     train = pd.read_csv(DataPath/'train'/'train.csv')
     segments = int(np.ceil(MaxRow / rows))
     eq_num = 0
-    head = np.inf
+    tail = np.inf
     for segment_num in tqdm(range(segments)):
         thisSegment = train[rows*segment_num:rows*(segment_num+1)].copy()
-        if head < thisSegment['time_to_failure'][rows*segment_num]:
+        head = thisSegment['time_to_failure'][rows*segment_num]
+        if tail < head:
             eq_num += 1
             thisSegment['eq_num'] = eq_num
         else:
-            min_ind = thisSegment['time_to_failure'].idxmin()
-            if min_ind != rows*(segment_num+1)-1:
-                thisSegment['eq_num'] = [eq_num]*(min_ind-rows*segment_num+1)+[eq_num+1]*(rows-(min_ind-rows*segment_num+1))
-                eq_num += 1
+            if segment_num < segments-1:
+                min_ind = thisSegment['time_to_failure'].idxmin()
+                if min_ind != rows*(segment_num+1)-1:
+                    thisSegment['eq_num'] = [eq_num]*(min_ind-rows*segment_num+1)+[eq_num+1]*(rows-(min_ind-rows*segment_num+1))
+                    eq_num += 1
+                else:
+                    thisSegment['eq_num'] = eq_num
+                tail = thisSegment['time_to_failure'][rows*(segment_num+1)-1]
             else:
                 thisSegment['eq_num'] = eq_num
-        head = thisSegment['time_to_failure'][rows*segment_num]
 
         thisSegment.to_csv(DestinationPath/'train_{}.csv'.format(segment_num),index=False)
 
