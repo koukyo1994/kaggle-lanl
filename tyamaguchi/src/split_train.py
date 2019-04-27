2c9eb83a03f2a5ff7938e11f992304e11e23f5a1
山口大器 <sunflower6432779@gmail.com@>
2019-04-27 18:46:13 +0900

    first commit

---
+++ tyamaguchi/src/split_train.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from tqdm import tqdm
+
+pd.options.display.precision = 15
+
+DataPath = Path('input')
+DestinationPath = DataPath/'train_split'
+DestinationPath.mkdir(exist_ok=True)
+header = ['acoustic_data','time_to_failure']
+MaxRow = 629145480
+rows = 150000
+
+
+def main():
+    train = pd.read_csv(DataPath/'train'/'train.csv')
+    segments = int(np.ceil(MaxRow / rows))
+    eq_num = 0
+    head = np.inf
+    for segment_num in tqdm(range(segments)):
+        thisSegment = train[rows*segment_num:rows*(segment_num+1)].copy()
+        if head < thisSegment['time_to_failure'][rows*segment_num]:
+            eq_num += 1
+            thisSegment['eq_num'] = eq_num
+        else:
+            min_ind = thisSegment['time_to_failure'].idxmin()
+            if min_ind != rows*(segment_num+1)-1:
+                thisSegment['eq_num'] = [eq_num]*(min_ind-rows*segment_num+1)+[eq_num+1]*(rows-(min_ind-rows*segment_num+1))
+                eq_num += 1
+            else:
+                thisSegment['eq_num'] = eq_num
+        head = thisSegment['time_to_failure'][rows*segment_num]
+
+        thisSegment.to_csv(DestinationPath/'train_{}.csv'.format(segment_num),index=False)
+
+if __name__ == '__main__':
+    main()
