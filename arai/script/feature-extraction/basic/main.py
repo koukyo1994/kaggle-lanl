0f3183c258145a93154ef2e3d558466af0c1d92f
koukyo1994 <arabiannight1994@yahoo.co.jp>
2019-04-30 17:38:56 +0900

    add feature-extraction

---
+++ arai/script/feature-extraction/basic/main.py
@@ -0,0 +1,51 @@
+import sys
+
+import numpy as np
+
+from pathlib import Path
+
+from scipy.io import savemat
+
+if __name__ == '__main__':
+    sys.path.append("../..")
+    sys.path.append("../")
+    sys.path.append("./")
+    sys.path.append("../common")
+
+    from basic import transform_ts
+    from denoising import highpass_denoise
+    from loader import data_generator
+
+    data_dir = Path("../../../../input/train_split")
+    generator = data_generator(data_dir)
+
+    feature_list = []
+    target_list = []
+    cnt = 0
+    while True:
+        try:
+            data = next(generator)
+            ts = data["acoustic_data"].values
+            time = data["time_to_failure"].values
+
+            duration = time[0] - time[-1]
+            nrows = len(data)
+            sample_rate = nrows * (1 / duration)
+            ts = highpass_denoise(
+                ts, low_cutoff=10000, sample_rate=sample_rate)
+            features = transform_ts(ts)
+            feature_list.append(features)
+            target_list.append(time[-1])
+            print(cnt)
+            cnt += 1
+        except StopIteration:
+            break
+
+    features = np.vstack(feature_list)
+    target = np.array(target_list)
+
+    save_dir = Path("../../../../features/basic")
+    save_dir.mkdir(exist_ok=True, parents=True)
+
+    mat = {"features": features, "target": target}
+    savemat(save_dir / "features.mat", mat)
---
+++ arai/script/feature-extraction/common/basic.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+from fastprogress import progress_bar
+
+
+def _min_max_transf(ts, min_data, max_data, range_needed=(-1, 1)):
+    ts = np.clip(ts, a_max=max_data, a_min=min_data)
+    if min_data < 0:
+        ts_std = (ts + abs(min_data)) / (max_data + abs(min_data))
+    else:
+        ts_std = (ts - min_data) / (max_data + abs(min_data))
+    if range_needed[0] < 0:
+        return ts_std * (
+            range_needed[1] + abs(range_needed[0])) + range_needed[0]
+    else:
+        return ts_std * (range_needed[1] - range_needed[0]) + range_needed[0]
+
+
+def transform_ts(ts, n_dim=150, min_max=(-1, 1)):
+    ts_std = _min_max_transf(ts, min_data=-128, max_data=127)
+    bucket_size = int(150000 / n_dim)
+    new_ts = []
+    for i in progress_bar(range(0, 150000, bucket_size)):
+        ts_range = ts_std[i:i + bucket_size]
+        mean = ts_range.mean()
+        std = ts_range.std()
+        std_top = mean + std
+        std_bot = mean - std
+        percentil_calc = np.percentile(ts_range, [0, 1, 25, 50, 75, 99, 100])
+        max_range = percentil_calc[-1] - percentil_calc[0]
+        relative_percentile = percentil_calc - mean
+        new_ts.append(
+            np.concatenate([
+                np.asarray([mean, std, std_top, std_bot, max_range]),
+                percentil_calc, relative_percentile
+            ]))
+    return np.asarray(new_ts)
