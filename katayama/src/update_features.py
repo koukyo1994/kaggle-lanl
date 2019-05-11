c80ed23d8a2c664dbda7667105d86b05c4385621
fozzhey <fozzhey@gmail.com>
2019-05-11 13:56:55 +0900

    Modify codes

--- katayama/src/update_features.py
+++ katayama/src/update_features.py
@@ -88,13 +88,47 @@ class Tsfresh(Feature):
 
         return record
 
+class Tsfresh2(Feature):
+    def calc_features(self, x, record):
+        percentiles = [1, 5, 10, 20, 25, 30, 40, 50, 60, 70, 75, 80, 90, 95, 99]
+        lags = [10, 100, 1000, 10000]
+
+        record['abs_energy'] = feature_calculators.abs_energy(x)
+        record['abs_sum_of_changes'] = feature_calculators.absolute_sum_of_changes(x)
+        record['count_above_mean'] = feature_calculators.count_above_mean(x)
+        record['count_below_mean'] = feature_calculators.count_below_mean(x)
+        record['mean_abs_change'] = feature_calculators.mean_abs_change(x)
+        record['mean_change'] = feature_calculators.mean_change(x)
+        record['var_larger_than_std_dev'] = feature_calculators.variance_larger_than_standard_deviation(x)
+
+        record['ratio_unique_values'] = feature_calculators.ratio_value_number_to_time_series_length(x)
+        record['first_loc_min'] = feature_calculators.first_location_of_minimum(x)
+        record['first_loc_max'] = feature_calculators.first_location_of_maximum(x)
+        record['last_loc_min'] = feature_calculators.last_location_of_minimum(x)
+        record['last_loc_max'] = feature_calculators.last_location_of_maximum(x)
+
+        for coeff, attr in product([1, 2, 3, 4, 5], ['real', 'imag', 'angle']):
+            record[f'fft_{coeff}_{attr}'] = list(feature_calculators.fft_coefficient(x, [{'coeff': coeff, 'attr': attr}]))[0][1]
+
+        record['long_strk_above_mean'] = feature_calculators.longest_strike_above_mean(x)
+        record['long_strk_below_mean'] = feature_calculators.longest_strike_below_mean(x)
+        record['cid_ce_0'] = feature_calculators.cid_ce(x, 0)
+        record['cid_ce_1'] = feature_calculators.cid_ce(x, 1)
+
+        for p in percentiles:
+            record[f'binned_entropy_{p}'] = feature_calculators.binned_entropy(x, p)
+
+        record['num_crossing_0'] = feature_calculators.number_crossing_m(x, 0)
+
+        return record
+
 def main():
     # Argument
     slide_size = 50000
     overwrite = False
 
     # Laod train data
-    train = util.read_train_data()
+    train = util.read_train_data(nrows=150000)
 
     tsfresh = Tsfresh(slide_size)
     tsfresh.run(train=train)
@@ -119,6 +153,15 @@ def main():
     tsfresh = Tsfresh(slide_size, series_type='ffti', denoising=True)
     tsfresh.run(train=train)
     tsfresh.save()
+
+
+    from tsfresh.feature_extraction import extract_features, EfficientFCParameters
+    params = EfficientFCParameters()
+
+    train_tmp = train.copy()
+    train_tmp = train_tmp[['acoustic_data']]
+    train_tmp['column_id'] = 1
+    tmp = extract_features(train_tmp, column_id='column_id', default_fc_parameters=params)
 
 
 if __name__ == '__main__':
--- katayama/src/util/util_functions.py
+++ katayama/src/util/util_functions.py
@@ -8,4 +8,5 @@ from paths import *
 
 
 def read_train_data(**kwargs):
+    pd.options.display.precision = 15
     return pd.read_csv(str(DATA_DIR/'input'/'train.csv'), dtype={'acoustic_data':np.float64, 'time_to_failure':np.float64}, **kwargs)
